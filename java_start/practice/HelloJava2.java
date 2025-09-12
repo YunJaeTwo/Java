581cc9d35cd60db59c8d241ac5e72ccf476a8706
0000 <a0000@0000ui-MacBookPro.local>
2025-09-13 01:53:06 +0900

    n

---
+++ java_start/practice/HelloJava2.java
@@ -0,0 +1,10 @@
+package java_start.practice;
+
+public class HelloJava2 {
+
+    public static void main(String[] args) {
+        System.out.println("HelloJava");
+        System.out.println("Hellojava2");
+        System.out.println("Hellojava3");
+    } 
+}
